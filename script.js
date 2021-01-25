97b8e696f1865372d708b19efff970ab8b545d0f
kadindev <developerwebmobile@outlook.com>
2021-01-24 21:52:01 -0300

    Parallax Effects

---
+++ script.js
@@ -0,0 +1,48 @@
+gsap.to('#bg',{
+    scrollTrigger: {
+        scrub: true
+    },
+    y: 200,
+    scale: 1.5
+})
+
+gsap.to('#moon',{
+    scrollTrigger: {
+        scrub: true
+    },
+    x: -400,
+    scale: 2
+})
+
+gsap.to('#cloud1',{
+    scrollTrigger: {
+        scrub: true
+    },
+    x: -400,
+})
+
+gsap.to('#cloud2',{
+    scrollTrigger: {
+        scrub: true
+    },
+    x: 300,
+})
+
+gsap.to('#jet',{
+    scrollTrigger: {
+        scrub: true
+    },
+    x: 1500,
+    y: 1000,
+    scale: 7
+})
+
+gsap.to('#ironman',{
+    scrollTrigger: {
+        scrub: true
+    },
+    x: 0,
+    y: -2000,
+    scale: 1.5,
+    rotate: -100
+})
